afe97988e1cef9b3929804e37c09eab953f5d5b2
WRR-Society <scholar@mail.dlut.edu.cn>
2026-03-02 21:15:50 +0800

    重构 HSC Studio 项目：更新 .gitignore 文件，调整命名空间，添加 Python 和 Go 后端支持

--- HSCIDE/ide/HSC Studio/HSC Studio/MainWindow.xaml.cs
+++ HSCIDE/ide/HSC Studio/HSC Studio/MainWindow.xaml.cs
@@ -9,16 +9,15 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace HSC_Studio
+namespace HSC_Studio;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
---
+++ HSCIDE/ide/backend/BBF/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from bbf!")
+
+
+if __name__ == "__main__":
+    main()
